ee8f3a6e7dcaec7b7b087891e3ddfda5cc9f0e0e
Thomas Liu <46125604+ZongYanLiu@users.noreply.github.com>
2021-09-11 14:05:19 +0800

    FIndMirTar
    A brand new approach to predict microRNA targets.

--- premirtar.py
+++ premirtar.py
@@ -15,7 +15,7 @@ with open(args.miRNA) as file, open(args.transcritome) as file:
     print("Full path to miRNA fasta file:", args.miRNA)
     print("Full path to transcriptome fasta file:", args.transcritome)
     print("===============================================\nBuilding BOWTIE database...")
-    os.system("bowtie-build -q %s bowtie" %(args.transcritome))
+    os.system("bowtie-build --threads 24 -q %s bowtie" %(args.transcritome))
     print("===============================================\nAnalyzing with BOWTIE...")
     os.system("bowtie -p 24 -a -f -l 5 -n 3 -e 360 -k 9999999 --fullref --norc --best bowtie %s > miRNA.bowtied" %(args.miRNA))
     print("===============================================\nCalculating by mirScore...")
@@ -24,7 +24,7 @@ with open(args.miRNA) as file, open(args.transcritome) as file:
     print("===============================================\nConverting to MirTarSite files...")
     df = pd.read_csv("%s/miRNA.bowtied.target.predict.tsv" %(pathlib.Path().absolute()), sep="\t")
     pd.set_option('display.max_rows', None)
-    all_mir = r"%s/output/all_mir.csv" %(pathlib.Path().absolute())
+    all_mir = r"%s/output/all_mir.fa" %(pathlib.Path().absolute())
     df.to_csv("%s/site_name.csv" %(pathlib.Path().absolute()), sep = "\t", index = False, header = False, columns=["Contig_Name"])
     df.to_csv("%s/targetsite.csv" %(pathlib.Path().absolute()), sep = "\t", index = False, header = False, columns=["Target_site"])
     df.to_csv("%s/tagsequence.csv" %(pathlib.Path().absolute()), sep = "\t", index = False, header = False, columns=["Tag_sequence"])
@@ -80,11 +80,11 @@ with open(args.miRNA) as file, open(args.transcritome) as file:
 
     print("===============================================\nAnalyzing with MirTarSite...")
     if __name__=='__main__':
-        os.system("python3 %s/mirtarsite/custom_predict.py %s/mirtarsite/state_dict/b16_lr0.001_embd100_rnnlayer1_rnnhidden100_drop0.3_ep47.pth %s/output/all_mir.fa %s/output/all_site.fa %s/output/interaction_pair.txt %s/mirtarsite/example/output_result.txt --cuda True" %(pathlib.Path().absolute(), pathlib.Path().absolute(), pathlib.Path().absolute(), pathlib.Path().absolute(), pathlib.Path().absolute(), pathlib.Path().absolute()))
-    df = pd.read_csv("%s/mirtarsite/example/output_result.txt" %(pathlib.Path().absolute()), sep="\t")
-    df.columns = ["microRNA", "target", "prediction"]
-    pd.set_option('display.max_rows', None)
-    with open("%s/output/mirRTarget_result_success.txt" %(pathlib.Path().absolute()), "w") as fd1, open("%s/output/miRTarget_result_false.txt" %(pathlib.Path().absolute()), "w") as fd2:
-        print(df[df["prediction"] == 1], file = fd1)
-        print(df[df["prediction"] == 0], file = fd2)
+        os.system("python3 %s/mirtarsite/custom_predict.py %s/mirtarsite/state_dict/b16_lr0.001_embd100_rnnlayer1_rnnhidden100_drop0.3_ep47.pth %s/output/all_mir.fa %s/output/all_site.fa %s/output/interaction_pair.txt %s/mirtarsite/example/output_result.txt --cuda True" %(pathlib.Path().absolute(), pathlib.Path().absolute(), pathlib.Path().absolute(), pathlib.Path().absolute(), pathlib.Path().absolute(), pathlib.Path().absolute()))   
+    df = pd.read_csv("%s/mirtarsite/example/output_result.txt" %(pathlib.Path().absolute()), sep="\t", names = ["microRNA", "target", "prediction"])
+    pd.set_option('display.max_rows', None,'display.max_colwidth', None)
+    df[df["prediction"] == 1].to_csv("%s/output/mirTarget_result_success.txt" %(pathlib.Path().absolute()), sep = "\t")
+    df[df["prediction"] == 0].to_csv("%s/output/mirTarget_result_false.txt" %(pathlib.Path().absolute()), sep = "\t")
+    os.system("rm ./output/*.fa")
+    os.system("rm ./output/interaction_pair.txt")
     print("Successfully analyze with miRTarget!\nYou can find your result at :%s/output/" %(pathlib.Path().absolute()))
